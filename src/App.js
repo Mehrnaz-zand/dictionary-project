7bcbbee4e207ebacba7f5d07c575e75270434d13
Mehrnaz-zand <zand.mehrnaz@gmail.com>
2021-11-29 17:44:52 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+    <div className="App">
+      <header className="App-header text-center">
+        <h1>
+        Dictionary App
+        </h1>
+      </header>
+    </div>
+    </div>
+  );
+}
+
+export default App;
